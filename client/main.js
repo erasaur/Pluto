ce34fd6eec5b1b0fd773d4f2ea06c56cbeaaf997
erasaur <erasaur@gmail.com>
2015-08-09 00:31:39 -0700

    Store preferred language in session on startup

---
+++ client/main.js
@@ -0,0 +1,14 @@
+if (Meteor.isCordova) {
+  var log = EMO.utils.log;
+
+  Meteor.startup(function () {
+    navigator.globalization.getPreferredLanguage(
+      function (language) {
+        Session.set('preferred_language', language.value);
+      },
+      function () {
+        log('Error getting language!!!1');
+      }
+    );
+  });
+};
