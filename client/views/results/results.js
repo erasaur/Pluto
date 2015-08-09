281d34de1c8dd0972adcdb73cf234a3508d34b0b
erasaur <erasaur@gmail.com>
2015-08-09 02:53:14 -0700

    Add results view

--- client/views/results/results.js
+++ client/views/results/results.js
@@ -1,9 +1,56 @@
+var helpers = EMO.helpers;
+var routes = helpers.routes;
+var storage = helpers.storage;
+var log = helpers.log;
+
+Template.results.onCreated(function () {
+  var self = this;
+  self._loading = new ReactiveVar(false);
+  self._results = new ReactiveVar();
+
+  var text = decodeURIComponent(routes.query('text'));
+
+  console.log(text);
+
+  if (text) {
+    self._loading.set(true);
+
+    Meteor.call('textToUni', text, function (error, result) {
+      if (error) {
+        showError();
+      } else {
+        if (result[0] === '\\') {
+          var newResult = {
+            original: text,
+            emoji: result
+          };
+
+          self._results.set(newResult);
+
+          // save to local persistent storage
+          storage.set(newResult);
+        }
+        else {
+          // TODO: translate with google translate
+          log('emoji translation failed bro. use gtranslate using language: ' + Session.get('preferred_language'));
+        }
+
+        self._loading.set(false);
+      }
+    });
+  }
+});
+
 Template.results.helpers({
-	//pass original and translation into here
-	original_speech: function() {
-		return 'I am happy'; // provide this;
-	},
-	emoticon_translation: function() {
-		return 'I am :-)'; // provide this
-	}
-})
+  loadingResults: function () {
+    var template = Template.instance();
+    return template._loading.get();
+  },
+  results: function () {
+    var template = Template.instance();
+    return template._results.get();
+  },
+  past_results: function () {
+    return storage.get();
+  }
+});
