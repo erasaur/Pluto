2ed3c184a8cb89d111e760d2ab196805a38e673c
erasaur <erasaur@gmail.com>
2015-08-09 03:08:54 -0700

    Add padding to result panels, show message if no emoji matches could be found

--- client/views/results/results.js
+++ client/views/results/results.js
@@ -16,23 +16,25 @@ Template.results.onCreated(function () {
     self._loading.set(true);
 
     Meteor.call('textToUni', text, function (error, result) {
+      var newResult = {
+        original: text
+      };
+
       if (error) {
         showError();
       } else {
         if (result[0] === '\\') {
-          var newResult = {
-            original: text,
-            emoji: result
-          };
-
+          newResult.emoji = result;
           self._results.set(newResult);
 
           // save to local persistent storage
           storage.set(newResult);
         }
         else {
-          // TODO: translate with google translate
           log('emoji translation failed bro. use gtranslate using language: ' + Session.get('preferred_language'));
+
+          // TODO: translate with google translate
+          self._results.set(newResult);
         }
 
         self._loading.set(false);
