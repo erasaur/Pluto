b0e956f0b7f4ba030dc50559f93025c28342e2d0
Wei Low <weiwlow@gmail.com>
2015-08-08 21:41:36 -0700

    text to uni fn

--- server/server.js
+++ server/server.js
@@ -3,6 +3,12 @@ var utils = EMO.utils;
 var Speech = Meteor.npmRequire('google-speech-api');
 var Future = Meteor.npmRequire('fibers/future');
 
+var dict = {};
+
+Meteor.startup(function() {
+  Meteor.call('makeDictionary', JSON.parse(Assets.getText('/private/emoji-processed.json')));
+});
+
 Meteor.methods({
   'audioToText': function (files) {
     check(files, [Object]);
@@ -27,7 +33,29 @@ Meteor.methods({
       // console.log(results[results.result_index]);
       // console.log(results[0].result[0].alternative);
     });
-
     return future.wait();
+  },
+  //Converts valid text to emoji
+  'textToUni': function (textString) {
+    var textArray = "I RODE A CAR TODAY".toUpperCase().split(" ");
+    var returnContent = "";
+    //var ucodes = []
+    textArray.forEach(function(i){
+      i = i.trim();
+      if (typeof dict[i] !== 'undefined') {
+        returnString += "\\" +dict[i] + " ";
+      }
+    });
+    if (returnContent) {
+      return returnContent;
+    }
+    else {
+      return textString;
+    }
+  },
+  'makeDictionary': function (jsonObject) {
+    jsonObject.forEach(function(x) {
+      dict[x.name] = x.unified;
+    });
   }
 });
