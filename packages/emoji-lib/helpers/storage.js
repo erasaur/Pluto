774616fcb8f8b8eaf1fee95446389f827fe60f6b
erasaur <erasaur@gmail.com>
2015-08-09 05:26:57 -0700

    Fix storage

--- packages/emoji-lib/helpers/storage.js
+++ packages/emoji-lib/helpers/storage.js
@@ -1,17 +1,25 @@
 var utils = EMO.utils;
+var log = EMO.helpers.log;
 var storage = EMO.helpers.storage = {};
 
 var STORAGE_NAME = 'pluto-storage';
 
 storage.get = function (key) {
   var store = localStorage.getItem(STORAGE_NAME);
+  try {
+    store = JSON.parse(store);
+  } catch (error) {
+    log('malformed localStorage...? failed json parse in get');
+    store = {};
+  }
+
   return key ? utils.get(store, key) : store;
 };
 
-storage.set = function (item) {
+storage.add = function (item) {
   var store = storage.get();
   store = store || [];
   store.push(item);
-  localStorage.setItem(STORAGE_NAME, store);
+  localStorage.setItem(STORAGE_NAME, JSON.stringify(store));
   return store;
 };
