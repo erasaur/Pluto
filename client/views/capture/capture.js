585262ab705b574163c337d91aa227c255902988
erasaur <erasaur@gmail.com>
2015-08-09 00:30:33 -0700

    Fix text to emoji call, add hamburger menu to navs

--- client/views/capture/capture.js
+++ client/views/capture/capture.js
@@ -1,28 +1,19 @@
+var log = EMO.utils.log;
+
+Template.capture.onCreated(function () {
+  this.results = new ReactiveVar();
+});
+
+Template.capture.helpers({
+  results: function () {
+    var template = Template.instance();
+    return template.results.get();
+  }
+});
+
 Template.capture.events({
   'click #js-capture': function (event, template) {
-    function captureSuccess (mediaFiles) {
-      Meteor.call('audioToText', mediaFiles, function (error, result) {
-        if (error) {
-          IonPopup.alert({
-            title: 'Error',
-            template: 'There was an error processing the audio',
-            okText: 'Ok'
-          });
-        } else {
-          var words = Meteor.call('textToUni', result);
-          if words[0]=='\\' {
-            //set the CSS Content
-          }
-          else {
-            //do translation
-          }
-
-          Router.go('results');
-        }
-      });
-    }
-
-    function captureError (error) {
+    function showError () {
       IonPopup.alert({
         title: 'Error',
         template: 'There was an error processing the audio',
@@ -30,10 +21,34 @@ Template.capture.events({
       });
     }
 
+    function captureSuccess (mediaFiles) {
+      Meteor.call('audioToText', mediaFiles, function (error, result) {
+        if (error) {
+          showError();
+        } else {
+          Meteor.call('textToUni', result, function (error, result) {
+            if (error) {
+              showError();
+            } else {
+              if (result[0] === '\\') {
+                template.results.set(result);
+              }
+              else {
+                // do translation
+                log('emoji translation failed bro. use gtranslate using language: ' + Session.get('preferred_language'));
+              }
+            }
+          });
+
+          Router.go('results');
+        }
+      });
+    }
+
+    function captureError (error) {
+      showError();
+    }
+
     navigator.device.capture.captureAudio(captureSuccess, captureError, { duration: 10 });
-    navigator.globalization.getPreferredLanguage(
-        function (language) {alert('language: ' + language.value + '\n');},
-        function () {alert('Error getting language\n');}
-    );
   }
 });
