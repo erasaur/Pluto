145a6c257cbb4b9600d309109451f29813ecd014
Wei Low <weiwlow@gmail.com>
2015-08-09 04:24:34 -0700

    updated dictionary of emojis

--- .scripts/dictionaryGenerator.js
+++ .scripts/dictionaryGenerator.js
@@ -3,19 +3,20 @@ var fs = require('fs');
 
 var arr = JSON.parse(fs.readFileSync('emoji_pretty.json'));
 
-var dict = [];
+var dict = {};
 
 var res = arr.map(function (emoji) {
-  var obj = {};
-  if (!emoji.name) {
-    var keyShortName = emoji.short_name.split("-").join(" ").toUpperCase();
-    obj[keyShortName] = emoji.unified;
-    dict.push(obj);
+  if (emoji.name) {
+    if (!dict[emoji.name]) {
+      dict[emoji.name] = emoji.unified;
+    }
   }
-  else {
-    obj[emoji.name] = emoji.unified;
-    dict.push(obj);
-  }
+  emoji.short_names.forEach(function (sn) {
+    var keyShortName = emoji.short_name.split("_").join(" ").toUpperCase();
+    if (!dict[keyShortName]) {
+      dict[keyShortName] = emoji.unified;
+    }
+  });
 });
 
 fs.writeFile('emoji_processed.json', JSON.stringify(dict), function (error) {
